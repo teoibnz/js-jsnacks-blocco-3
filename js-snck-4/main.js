e7c385ab9f94ea1843a848987b7a7a6e0fda3ef1
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-05 11:35:19 +0100

    finalizzazione snack 4 blocco 3

--- js-snck-4/main.js
+++ js-snck-4/main.js
@@ -1,11 +1,11 @@
 
 
-function checkIfIsEven (num1, num2) {
-    if  (num1+ num2 % 2 == 0){
+function evenSum (num1, num2) {
+    if ((num1 + num2) % 2 === 0){
         return true;
-    } else {
-        check = false;
-    }
+    } else{
+        return false;
+    }    
 };
-checkIfIsEven = (1+2)
-console.log(checkIfIsEven);
+
+console.log(evenSum(2,3))
