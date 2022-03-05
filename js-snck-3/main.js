7bb8e80a254e6880c2b533dceae023ff46dcd8b2
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-05 11:04:39 +0100

    finalizzazione snack 3 blocco 3

--- js-snck-3/main.js
+++ js-snck-3/main.js
@@ -1,25 +1,42 @@
-let greenBox = document.createElement("div").classList.add("bg-green");
+// Crea due tag div con due id diversi:
+//    un div avrà il testo colorato di rosso mentre l'altro di verde.
+//    Partendo da un array di numeri, creo una funzione che controlla se siano pari o dispari.
+//    Se sono pari li stampo nell'div "rosso",
+//    se sono dispari li stampo nell'div "verde".
 
-let redBox = document.createElement('div').classList.add("bg-red");
 
+
+
+let greenBox = document.createElement("div");
+    greenBox.classList.add("green");
+
+let redBox = document.createElement('div');
+    redBox.classList.add("red");
 
 const mainElement = document.querySelector('main');
 
-mainElement.append(greenBox, redBox);
+let finalRedBox = mainElement.appendChild(redBox);
+let finalGreenBox = mainElement.appendChild(greenBox);
 
 
+const numbers = ['5', '4',];
 
-numbers = ['5', '4',];
+function isEven(number){
+    if ( number % 2 == 0 ){
+        return true;
+    }
+    return false;
+}
 
-function numbersCheck (numbers) {
-    if (numbers % 2 == 0) {
-        return greenBox.innerHTML += numbers[i];
+for ( let i = 0; i < numbers.length; i++) {
+    if (isEven(numbers[i])) {
+        greenBox.innerHTML += numbers[i] + ' è pari!';
 
     } else {
-        redBox.innerHTML += numbers[i];
+        redBox.innerHTML += numbers[i] + ' è dispari!';
+
     }
-};
+}
 
 
-console.log(numbersCheck(4));
 
